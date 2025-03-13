cae88427cc43f28bf62669f9dc8cbaf30c4b9188
nyanpasu3 <kiraqwertynekoneko@gmail.com>
2025-03-13 16:43:07 +0200

    checked how dynamic routes works in next.js and fixed an error with add async/await

---
+++ src/app/feed/[videoId]/page.tsx
@@ -0,0 +1,11 @@
+interface PageProps {
+  params: Promise<{ videoId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { videoId } = await params;
+
+  return <div>Video Id: {videoId}</div>;
+};
+
+export default Page;
---
+++ src/app/feed/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Feed page!</div>;
+};
+
+export default Page;
