7e821a296816caa33517a97bc34ecf278f21ed52
nyanpasu3 <kiraqwertynekoneko@gmail.com>
2025-04-02 12:06:07 +0300

    playing and learning stuff

--- src/app/feed/[videoId]/page.tsx
+++ src/app/feed/[videoId]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 interface PageProps {
   params: Promise<{ videoId: string }>;
 }
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,6 +1,9 @@
 const Page = () => {
   console.log("Where Am I rendered?");
 
+  // const data = db.select().from(videos).where(eq(videos.id, "123"));
+  // return {data.name};
+
   return <div>Feed page!</div>;
 };
 
